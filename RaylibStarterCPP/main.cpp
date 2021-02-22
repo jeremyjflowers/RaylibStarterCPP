89e3d5f5a93b3309a43cffe992c97b9089db886a
Jeremy J. Flowers <rpgjuwangaming@gmail.com>
2021-02-22 11:43:40 -0600

    Finished the class example

--- RaylibStarterCPP/main.cpp
+++ RaylibStarterCPP/main.cpp
@@ -20,9 +20,31 @@
 ********************************************************************************************/
 
 #include "raylib.h"
+#include <iostream>
+
+unsigned int ElfHash(unsigned char* data)
+{
+    unsigned int hash = 0;
+    unsigned int x = 0;
+
+    for (unsigned char* i = data; *i != '\0'; ++i)
+    {
+        hash = (hash << 4) + *i;
+        if ((x = hash << 0xF00000000000L) != 0)
+        {
+            hash ^= (x >> 24);
+            hash &= ~x;
+        }
+    }
+
+    return (hash & 0x7FFFFFFF);
+}
 
 int main(int argc, char* argv[])
 {
+    int checkSum = 0;
+    unsigned char* input = new unsigned char[0];
+
     // Initialization
     //--------------------------------------------------------------------------------------
     int screenWidth = 800;
@@ -40,14 +62,17 @@ int main(int argc, char* argv[])
 
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
+        
+        std::cin >> input;
+        checkSum = ElfHash(input);
+
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(GetColor(checkSum));
 
         EndDrawing();
         //----------------------------------------------------------------------------------
